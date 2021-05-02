4a70364ddb0313414587e4f6fdc112422e07797a
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-05-03 10:54:35 +1200

    Base

---
+++ 00_FRC_base_v1.py
@@ -0,0 +1,72 @@
+
+# Functions goes here
+
+# check that product name is not blank
+
+
+
+
+# check quantity is more than 0
+
+
+
+
+# string checker
+
+
+
+
+
+
+# *** MAIN ROUTINE***
+
+# Set up dictionaries / lists needed to hold data
+
+
+
+# Ask the user if they have used the program before & show instruction if necessary
+
+
+
+# Loop to get product details (variable cost)
+ # start of loop
+
+# initialise loop so that it runs at least once
+
+
+
+
+
+# --- End of variable cost ---
+
+
+
+# ----- Loop to ask for fixed costs -----
+
+
+
+
+# --- End of fixed cost ---
+
+
+# Profit goal
+
+# Ask for nearest rounding prefered
+
+# Selling price
+
+
+# ~~PRINTING~~
+
+# variable cost table displaying each item, price and cost -total
+
+
+# fixed cost table displaying each item, cost - total
+
+
+# total cost (adding fixed and variable cost)
+
+
+# Profit and sales targets
+
+# Pricing

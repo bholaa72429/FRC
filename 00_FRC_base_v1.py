7bf4dbe2948c478fba46f78651b05ebf4ac5bf44
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-05-04 09:54:51 +1200

    num check func added

--- 00_FRC_base_v1.py
+++ 00_FRC_base_v1.py
@@ -4,16 +4,26 @@
 # check that product name is not blank
 
 
+# checks that input is a float or an integer which is more zero.
+# has custom error messages for int/float
+def num_check(question, error, num_type):
+    valid = False
 
+    while not valid:
 
-# check quantity is more than 0
+        try:
+            response = num_type(input(question))
 
+            # if response is less or = to 0
+            if response <= 0:
+                print(error)
+            else:
+                return response
 
-
+        except ValueError:
+            print(error)
 
 # string checker
-
-
 
 
 

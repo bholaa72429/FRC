b0e81dfb63fda1825bdcab93a283e7fb31d8ec98
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-05-05 14:49:40 +1200

    developing

--- 02_yes_no_checker.py
+++ 02_yes_no_checker.py
@@ -1,2 +1,23 @@
-# function goes here
+# yes/no function goes here
 
+def yes_no(question):
+
+    to_check = ["yes","no"]
+
+    valid = False
+    while not valid:
+
+        # ask question and put response in lowercase
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+        print("please answer yes / no")
+
+# Main routine
+for item in range (0,6):
+    want_help = yes_no("Do you want to read instructions ? ")
+    print("You said ")
